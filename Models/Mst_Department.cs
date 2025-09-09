3f2785432c065a197b873d5a4cd7bfd4250f97e8
Prithvi_Rathod <rathodprithvi24@gmail.com>
2025-09-09 11:20:22 +0530

    Department modal

--- Models/Mst_Department.cs
+++ Models/Mst_Department.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace MyBlazorApp.Models
 {
     [Table("Mst_Department")]
-    public class Mst_Department
+    public class    Mst_Department
     {
         [Key]
         public int Id { get; set; }
