dd1653fc5aefe6480821a9d39027ce12fdc2c1e6
smartwaypharma <harshad@smartwaypharma.co.uk>
2025-10-01 17:22:59 +0100

    Table change

--- Data/EMSContext.cs
+++ Data/EMSContext.cs
@@ -13,6 +13,7 @@ namespace MyBlazorApp.Data
         public virtual DbSet<Mst_Department> Mst_Department { get; set; }
         public virtual DbSet<ErrorLog> ErrorLogs { get; set; }
         public virtual DbSet<Mst_Menu> Mst_Menu { get; set; }
+        public virtual DbSet<Mst_Menu_New> Mst_Menu_New { get; set; }
         public virtual DbSet<Mst_Right> Mst_Right { get; set; }
         public virtual DbSet<Mst_UserRights> Mst_UserRight { get; set; }
         public virtual DbSet<Mst_Role> Mst_Role { get; set; }
---
+++ Models/Mst_Menu_New.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyBlazorApp.Models
+{
+    [Table("Mst_Menu_New")]
+    public class Mst_Menu_New
+    {
+        [Key]
+        public int MID { get; set; }
+
+        public int? PMID { get; set; }
+
+        [StringLength(200)]
+        public string? MenuName { get; set; }
+
+        [StringLength(200)]
+        public string? MenuUrl { get; set; }
+
+        [StringLength(200)]
+        public string? MenuDesc { get; set; }
+
+        [StringLength(500)]
+        public string? MainMenu { get; set; }
+
+        public int? SortOrder { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        public int? DeletedBy { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public bool? IsFrontMenu { get; set; }
+    }
+}
