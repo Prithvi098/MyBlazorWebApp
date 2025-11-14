67a182bfdcbcb34af3e974ec06d42a97d118b507
smartwaypharma <harshad@smartwaypharma.co.uk>
2025-11-14 17:32:36 +0000

    some changes

--- Models/Mst_Department.cs
+++ Models/Mst_Department.cs
@@ -6,20 +6,27 @@ namespace MyBlazorApp.Models
     [Table("Mst_Department")]
     public class Mst_Department
     {
-        [Key]
         public int Id { get; set; }
 
+        [StringLength(500)]
         public string? DepartmentName { get; set; }
 
-        public bool? IsActive { get; set; }
+        public bool? Isactive { get; set; }
 
         public int? CreatedBy { get; set; }
+
         public DateTime? CreatedOn { get; set; }
 
         public int? UpdatedBy { get; set; }
+
         public DateTime? UpdatedOn { get; set; }
 
         public int? DeletedBy { get; set; }
+
         public DateTime? DeletedOn { get; set; }
+
+        public bool? IsSubDepartment { get; set; }
+
+        public int? ParentDepartmentId { get; set; }
     }
 }
