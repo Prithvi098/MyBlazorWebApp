4b06ff857a3777f738a3276df78aa1199152635b
smartwaypharma <harshad@smartwaypharma.co.uk>
2025-11-24 17:49:43 +0000

    fixed user rights

--- Program.cs
+++ Program.cs
@@ -141,6 +141,7 @@ builder.Services.AddScoped<MenuService>();
 builder.Services.AddScoped<DataContext>();
 builder.Services.AddScoped<PunchService>();
 builder.Services.AddScoped<JwtTokenService>();
+builder.Services.AddScoped<UserPermissionService>();
 
 builder.Services.AddScoped<Helper>();
 
